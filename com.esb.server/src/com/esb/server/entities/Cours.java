b5ad62d265dde72a07031c021aac572fc9978045
Alexandre Versmee <versmee.alexandre@gmail.com>
2015-11-28 19:48:40 -0600

    Modification entity cours

--- com.esb.server/src/com/esb/server/entities/Cours.java
+++ com.esb.server/src/com/esb/server/entities/Cours.java
@@ -1,6 +1,51 @@
 package com.esb.server.entities;
 
-public class Cours extends AFile {
-	Long idModule;
-	Long idCreator;
+import org.mongodb.morphia.annotations.Entity;
+
+@Entity
+public class Cours extends MediaGeneric
+{
+	/*
+	###############################
+	#         Attributes          #
+	###############################
+	*/
+	Long idModule; /* ça serait pas plus private Module idModule */
+	Long idCreator; /* ça serait pas plus private User idCreator */
+	/*
+	###############################
+	#         Getter              #
+	###############################
+	*/
+	public Long getIdModule() {
+		return idModule;
+	}
+	public Long getIdCreator() {
+		return idCreator;
+	}
+	/*
+	###############################
+	#         Setter              #
+	###############################
+	*/
+	public void setIdModule(Long idModule) {
+		this.idModule = idModule;
+	}
+	public void setIdCreator(Long idCreator) {
+		this.idCreator = idCreator;
+	}
+	/*
+	###############################
+	#        Other Methods        #
+	###############################
+	*/
+	public String toString()
+	{
+		String attributes;
+		
+		attributes = "[idCreator : "+this.getIdCreator()+
+					"idModule : "+this.getIdModule()+
+					"]";
+		return attributes;
+	}
 }
